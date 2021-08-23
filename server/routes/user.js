a65e173c691874f06cb0599652e361c60fbc4fa2
saaranshM <saaransh.menon2000@gmail.com>
2021-08-23 16:45:18 +0530

    added login route

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const db = require("../db");
 const jwtGenerator = require("../utils/jwtGenerator");
+const addTokenToArray = require("../utils/addTokenToArray");
 
 router.post("/register", async (req, res) => {
   // destructuring  request body
@@ -36,11 +37,8 @@ router.post("/register", async (req, res) => {
     // generate jwt token for user
     const token = jwtGenerator({ user_id: newUserCredntials[0].user_id });
 
-    await db("user_login")
-      .where({ user_id: newUserCredntials[0].user_id })
-      .update({
-        tokens: db.raw("array_append(tokens, ?)", [token]),
-      });
+    // call function to add token to tokens array
+    await addTokenToArray(newUserCredntials[0].user_id, token);
 
     // saving personal user details to db
     await db("user_profile").insert({
@@ -50,6 +48,49 @@ router.post("/register", async (req, res) => {
     });
 
     // send user the token after registration
+    res.status(201).json({ token });
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(500);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  // destructuring request body to get email and password for login
+  const { email, password } = req.body;
+  try {
+    // get user from database
+    const user = await db("user_login").select().where({ user_email: email });
+
+    // send not found if user is not found
+    if (!user[0]) {
+      return res.status(401).json({
+        error: "not-found",
+        message: "User does not exist",
+      });
+    }
+
+    // check if password is valid
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      user[0].user_password
+    );
+
+    // send invalid cresentials if password is incorrect
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        error: "invalid-credentails",
+        message: "email or password is incorrect",
+      });
+    }
+
+    // generate jwt token for user
+    const token = jwtGenerator({ user_id: user[0].user_id });
+
+    // call function to add token to tokens array
+    await addTokenToArray(user[0].user_id, token);
+
+    // send token if email is valid
     res.status(201).json({ token });
   } catch (error) {
     console.log(error.message);
---
+++ server/utils/addTokenToArray.js
@@ -0,0 +1,11 @@
+const db = require("../db");
+
+const addTokenToArray = async (userId, token) => {
+  await db("user_login")
+    .where({ user_id: userId })
+    .update({
+      tokens: db.raw("array_append(tokens, ?)", [token]),
+    });
+};
+
+module.exports = addTokenToArray;
