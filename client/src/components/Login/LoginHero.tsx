584afb284f8ee53e4ddcfd9e06a26affec125231
Saaransh <saaransh.menon2000@gmail.com>
2021-09-08 23:16:54 +0530

    added hover effect in login page

--- client/src/components/Login/LoginHero.tsx
+++ client/src/components/Login/LoginHero.tsx
@@ -1,7 +1,11 @@
-import { FC } from "react";
+import { FC, MutableRefObject } from "react";
 import loginImg from "../../assets/img/login-img.png";
 
-const LoginHero: FC = () => {
+interface LoginHeroProps {
+  divRef: MutableRefObject<HTMLDivElement>;
+}
+
+const LoginHero: FC<LoginHeroProps> = ({ divRef }) => {
   return (
     <div className="login-hero">
       <div className="login-hero_container">
@@ -9,7 +13,9 @@ const LoginHero: FC = () => {
           Lightning fast way to manage your expenses
           <span> Start Now!</span>
         </div>
+
         <img src={loginImg} alt="Login Hero" className="login-hero_img" />
+        <div ref={divRef} className="cursor"></div>
       </div>
     </div>
   );
--- client/src/components/Login/LoginScreen.tsx
+++ client/src/components/Login/LoginScreen.tsx
@@ -1,13 +1,23 @@
-import { FC } from "react";
+import { FC, Fragment, useRef, MouseEvent } from "react";
 import LoginHero from "./LoginHero";
 
 interface LoginScreenProps {}
 
 const LoginScreen: FC<LoginScreenProps> = () => {
+  const cursorRef = useRef(document.createElement("div"));
+
+  console.log(cursorRef);
+
+  const handleMouseMove = (e: MouseEvent) => {
+    cursorRef.current.style.left = e.clientX + "px";
+    cursorRef.current.style.top = e.clientY + "px";
+  };
   return (
-    <section className="login">
-      <LoginHero />
-    </section>
+    <Fragment>
+      <section onMouseMove={handleMouseMove} className="login">
+        <LoginHero divRef={cursorRef} />
+      </section>
+    </Fragment>
   );
 };
 
