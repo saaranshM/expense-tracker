a7bef8f1f5ba84adbd9e4625bcf68d33abbcee45
Saaransh <saaransh.menon2000@gmail.com>
2021-09-08 15:47:14 +0530

    added login hero

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Login from "./views/Login";
+import "./scss/index.scss";
 
 function App() {
   return (
